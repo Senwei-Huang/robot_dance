82dd9201c1f12d66233afad630a51c1ae7a9684a
Senwei-Huang <15207847842@163.com>
2024-12-10 20:07:27 +0800

    actions

--- legged_gym/legged_gym/envs/base/legged_robot.py
+++ legged_gym/legged_gym/envs/base/legged_robot.py
@@ -127,7 +127,7 @@ class LeggedRobot(BaseTask):
         Args:
             actions (torch.Tensor): Tensor of shape (num_envs, num_actions_per_env)
         """
-        actions = self.reindex(actions)
+        actions.to(self.device)
         clip_actions = self.cfg.normalization.clip_actions / self.cfg.control.action_scale
         self.actions = torch.clip(actions, -clip_actions, clip_actions).to(self.device)
         
