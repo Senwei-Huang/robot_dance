24879ebc8cfd72ca1172681cfb280df45ade450e
Senwei-Huang <15207847842@163.com>
2024-12-10 15:43:24 +0800

    格式

--- use_cmd.py
+++ use_cmd.py
@@ -6,8 +6,9 @@ python legged_gym/legged_gym/scripts/train.py --task=panda7_arm_beat
 
 
 # play
-python legged_gym/legged_gym/scripts/play_panda.py --task=panda7_arm_swing --num_envs=1
-python legged_gym/legged_gym/scripts/play_panda.py --task=panda7_arm_swing --num_envs=1
+python legged_gym/legged_gym/scripts/play_panda.py --task=panda7_arm_swing
+
+python legged_gym/legged_gym/scripts/play_panda.py --task=panda7_arm_beat --num_envs=1
 
 # log
 ~/robot_dance/legged_gym/logs/panda7_fixed_gripper_beat$ tensorboard --logdir Dec04_22-34-54_/
